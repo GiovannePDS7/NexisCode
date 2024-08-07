e0d7c33f7dad73b72667849786afe2f25fe33079
Henriqueluz21 <henrique.rique200611@gmail.com>
2024-08-07 14:59:29 -0300

    add integrantes (faltando ajustes)

--- src/app/components/integrantes/integrantes.component.ts
+++ src/app/components/integrantes/integrantes.component.ts
@@ -17,6 +17,18 @@ export class IntegrantesComponent implements AfterViewInit {
     { name: 'Isabella Amaro', role: 'UI/UX Designer', image: '../../../assets/integrantes/Isabella.png', github: 'Isabellinhaa', linkGithub: 'https://github.com/Isabellinhaa' },
     { name: 'Pedro Soares', role: 'DataBase Administrator', image: '../../../assets/integrantes/Pedro.png', github: 'PedroSoares22', linkGithub: 'https://github.com/PedroSoares22' },
   ];
+  
+  members2 = [
+    { name: 'João Eduardo', role: 'Front-end Developer', image: '../../../assets/integrantes/Joao.png', github: 'juedu006', linkGithub: 'https://github.com/juedu006' },
+    { name: 'Henrique Lopes', role: 'Back-end Developer', image: '../../../assets/integrantes/Henrique.png', github: 'Henriqueluz21', linkGithub: 'https://github.com/Henriqueluz21' },
+    { name: 'Anderson A.', role: 'DataBase Administrator', image: '../../../assets/integrantes/Anderson.png', github: 'Anderson-Armando', linkGithub: 'https://github.com/Anderson-Armando' },
+  ];
+
+  members3 = [
+    { name: 'Giovanne P.', role: 'Full-stack Developer', image: '../../../assets/integrantes/Giovanne.png', github: 'GiovannePDS7', linkGithub: 'https://github.com/GiovannePDS7' },
+    { name: 'Isabella Amaro', role: 'UI/UX Designer', image: '../../../assets/integrantes/Isabella.png', github: 'Isabellinhaa', linkGithub: 'https://github.com/Isabellinhaa' },
+    { name: 'Pedro Soares', role: 'DataBase Administrator', image: '../../../assets/integrantes/Pedro.png', github: 'PedroSoares22', linkGithub: 'https://github.com/PedroSoares22' },
+  ];
 
   ngAfterViewInit() {
     this.addDragFunctionality();
