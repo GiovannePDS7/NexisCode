84676f20ae7b41d666d46cef5727ee194ea5d92e
Giovanne Pagano <giovanne3282@gmail.com>
2024-08-08 21:55:10 -0300

    ajustes

--- src/app/components/darkMode/dark-mode.service.ts
+++ src/app/components/darkMode/dark-mode.service.ts
@@ -15,33 +15,62 @@ export class DarkModeService {
     const h1txtShadow = document.querySelectorAll('.txt-destaque');
     const ondas = document.querySelectorAll('.onda') as NodeListOf<HTMLImageElement>;
     const logo = document.querySelectorAll('.logoEmpresa') as NodeListOf<HTMLImageElement>;
+    const linha = document.querySelectorAll(".linha");
+    const Mvv = document.querySelectorAll(".mvv");
+    const Qmsomos_txt = document.querySelectorAll('.qm-somos');
+    const ContainerQmSomos = document.querySelectorAll('.grandContainerQuemSomos');
 
     if (isDarkMode) {
       document.body.classList.add('dark-theme');
-
+      linha.forEach(e => {
+        e.classList.add('linha-white');
+      })
       h1txtShadow.forEach(e => {
         e.classList.add('txtShadow-dark-theme')
       })
-     ondas.forEach(e =>{
-      e.src = '../../../assets/elementos/ondaEscura.png';
-     })
-     logo.forEach(e =>{
-      e.src = '../../../assets/Logo/Logo_branca.png';
-     })
+      ondas.forEach(e => {
+        e.src = '../../../assets/elementos/ondaEscura.png';
+      })
+      logo.forEach(e => {
+        e.src = '../../../assets/Logo/Logo_branca.png';
+      })
 
+      Mvv.forEach(e => {
+        e.classList.add('Mvv-white')
+      })
+      Qmsomos_txt.forEach(e => {
+        e.classList.add('Mvv-white');
+      })
+      ContainerQmSomos.forEach(e => {
+        e.classList.add('ContainerQmSomosBg');
+      })
     } else {
       document.body.classList.remove('dark-theme');
+
+      Mvv.forEach(e => {
+        e.classList.remove('Mvv-white')
+      })
+
+      linha.forEach(e => {
+        e.classList.remove('linha-white');
+      })
 
       h1txtShadow.forEach(e => {
         e.classList.remove('txtShadow-dark-theme')
       })
 
-      ondas.forEach(e =>{
+      ondas.forEach(e => {
         e.src = '../../../assets/elementos/ondaClara.png';
-       })
-       logo.forEach(e =>{
+      })
+      logo.forEach(e => {
         e.src = '../../../assets/Logo/Logo_preta.png';
-       })
+      })
+      Qmsomos_txt.forEach(e => {
+        e.classList.remove('Mvv-white');
+      })
+      ContainerQmSomos.forEach(e => {
+        e.classList.remove('ContainerQmSomosBg');
+      })
     }
   }
 }
